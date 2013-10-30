4c287294697085792e63e5badd8fc9a1e2efe603
Charlie Cheever <ccheever@gmail.com>
2013-10-30 12:39:57 -0700

    Using bower for React now

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,48 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    
     pkg: grunt.file.readJSON('package.json'),
-    browserify: {
-      'build/index.js': ['src/index.js']
+
+
+    react: {
+      app: {
+        options: {
+          extension:    'jsx',  // Default
+          ignoreMTime:  false, // Default
+        },
+        files: {
+          'build/jsx': 'src/'
+        },
+      },
     },
+
+    browserify: {
+      all: {
+        src: ['src/index.js', 'build/jsx/hello.js'],
+        dest: 'build/js/index.js',
+        options: {
+          transform: ['debowerify']
+        },
+      }
+    },
+
+    copy: {
+      main: {
+        files: [
+          {expand: true, src: ['*.py'], dest: 'build/bin/', cwd: 'src/'}
+        ]
+      }
+    }
+
+
   });
 
 
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-react');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
 
-  grunt.registerTask("default", ["browserify"]);
+  grunt.registerTask("default", ["react", "browserify", "copy"]);
 }
 
--- src/server.py
+++ src/server.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python
+
+import os
+
 import tornado.ioloop
 import tornado.web
+
+def relative_path(fn):
+    return os.path.abspath(os.path.sep.join(os.path.abspath(__file__).split(os.path.sep)[:-1]) + os.path.sep + fn)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -7,21 +14,27 @@ class MainHandler(tornado.web.RequestHandler):
 <html>
 <head>
 <title>JS Autoreload Test</title>
-<script src="/index.js" type="text/javascript"></script>
 </head>
 <body>
 What does the fox say?
+<script src="/react.js" type="text/javascript"></script>
+<script src="/index.js" type="text/javascript"></script>
 </body>
 </html>
 """)
 
 class JSHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write(file("../build/index.js").read())
+        self.write(file(relative_path("../js/index.js")).read())
+
+class ReactHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(file(relative_path("../../bower_components/react/react.min.js")).read())
 
 application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/index.js", JSHandler),
+    (r"/react.js", ReactHandler),
 ])
 
 if __name__ == "__main__":
